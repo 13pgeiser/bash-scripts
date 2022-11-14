babffb9b7a9eadb2a746e022bad92548f44e5661
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2022-11-14 16:19:40 +0100

    Debian: do strictly nothing if apt-get is not available in the PATH

--- helpers_debian.sh
+++ helpers_debian.sh
@@ -5,12 +5,16 @@
 # https://github.com/13pgeiser/bash-scripts.git
 
 install_debian_packages() { #helpmsg: Install a list of debian packages using sudo
-	check_commands dpkg-query apt-get sudo
-	local package
-	for package in "$@"; do
-		if ! dpkg-query -f '${Status}' -s "$package" | grep 'install ok' 2>/dev/null 1>/dev/null; then
-			echo "Installing $package"
-			sudo apt-get -y install "$package"
-		fi
-	done
+	if [ -x "$(command -v apt-get)" ]; then
+		check_commands dpkg-query sudo
+		local package
+		for package in "$@"; do
+			if ! dpkg-query -f '${Status}' -s "$package" | grep 'install ok' 2>/dev/null 1>/dev/null; then
+				echo "Installing $package"
+				sudo apt-get -y install "$package"
+			fi
+		done
+	else
+		echo "apt-get not found. Ignoring installation of $*"
+	fi
 }
