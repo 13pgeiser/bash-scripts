327c41ae68f095422ed85f90e60bbc404fac76f9
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2025-09-11 20:29:20 +0200

    Use ':' in chmod

--- helpers_docker.sh
+++ helpers_docker.sh
@@ -87,7 +87,7 @@ ARG GID=1000
 RUN groupadd -g $GID -o $USER
 RUN useradd -m -u $UID -g $GID -o -s /bin/bash $USER
 RUN mkdir -p /work
-RUN chown -R ${USER}.${USER} /work
+RUN chown -R ${USER}:${USER} /work
 EOF
 }
 
